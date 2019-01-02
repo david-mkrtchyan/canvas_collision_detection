61f84a8ee00d3c9f23e6017df2729dd63ed99600
david_yan <david@fifth-llc.com>
2019-01-03 03:09:06 +0400

    final version

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -113,7 +113,7 @@ var mouse = {
     y: innerHeight / 2
 };
 
-var colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66'];
+var colors = ['#2185C5', '#7ECEFD', '#FC0', '#FF7F66'];
 
 // Event Listeners
 addEventListener('mousemove', function (event) {
@@ -129,32 +129,85 @@ addEventListener('resize', function () {
 });
 
 // Objects
-function Object(x, y, radius, color) {
+function Particle(x, y, radius, color) {
     this.x = x;
     this.y = y;
+    this.velocity = {
+        x: Math.random() - 0.5,
+        y: Math.random() - 0.5
+    };
     this.radius = radius;
     this.color = color;
+    this.mass = 1;
+    this.opacity = 0;
 }
 
-Object.prototype.draw = function () {
+Particle.prototype.draw = function () {
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.save();
+    c.globalAlpha = this.opacity;
     c.fillStyle = this.color;
     c.fill();
+    c.restore();
+    c.strokeStyle = this.color;
+    c.stroke();
     c.closePath();
 };
 
-Object.prototype.update = function () {
-    this.draw();
+Particle.prototype.update = function (particles) {
+    for (var i = 0; i < particles.length; i++) {
+        this.draw();
+        if (this === particles[i]) continue;
+        if (_utils2.default.getDistance(this.x, this.y, particles[i].x, particles[i].y) - this.radius * 2 < 0) {
+            _utils2.default.resolveCollision(this, particles[i]);
+        }
+
+        if (this.x - this.radius <= 0 || this.x + this.radius >= innerWidth) {
+            this.velocity.x = -this.velocity.x;
+        }
+
+        if (this.y - this.radius <= 0 || this.y + this.radius >= innerHeight) {
+            this.velocity.y = -this.velocity.y;
+        }
+
+        if (_utils2.default.getDistance(mouse.x, mouse.y, this.x, this.y) < 15 && this.opacity < 0.2) {
+            this.opacity += 0.01;
+        } else if (this.opacity > 0) {
+            this.opacity -= 0.01;
+            this.opacity = Math.max(0, this.opacity);
+        }
+
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+    }
 };
 
 // Implementation
-var objects = void 0;
-function init() {
-    objects = [];
+var particles = [];
 
-    for (var i = 0; i < 400; i++) {
-        // objects.push();
+function init() {
+    particles = [];
+    initParticles(30);
+}
+
+function initParticles(count) {
+    for (var i = 0; i < count; i++) {
+        var radius = 30;
+        var x = _utils2.default.randomIntFromRange(radius, canvas.width - radius);
+        var y = _utils2.default.randomIntFromRange(radius, canvas.height - radius);
+        var color = _utils2.default.randomColor(colors);
+
+        if (i !== 0) {
+            for (var j = 0; j < particles.length; j++) {
+                if (_utils2.default.getDistance(x, y, particles[j].x, particles[j].y) - radius * 2 < 0) {
+                    x = x = _utils2.default.randomIntFromRange(radius, canvas.width - radius);
+                    y = _utils2.default.randomIntFromRange(radius, canvas.height - radius);
+                    j = -1;
+                }
+            }
+        }
+        particles.push(new Particle(x, y, radius, color));
     }
 }
 
@@ -162,11 +215,9 @@ function init() {
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, canvas.width, canvas.height);
-
-    c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y);
-    // objects.forEach(object => {
-    //  object.update();
-    // });
+    particles.forEach(function (particle) {
+        return particle.update(particles);
+    });
 }
 
 init();
@@ -192,14 +243,59 @@ function randomColor(colors) {
     return colors[Math.floor(Math.random() * colors.length)];
 }
 
-function distance(x1, y1, x2, y2) {
+function getDistance(x1, y1, x2, y2) {
     var xDist = x2 - x1;
     var yDist = y2 - y1;
 
     return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
 }
 
-module.exports = { randomIntFromRange: randomIntFromRange, randomColor: randomColor, distance: distance };
+function rotate(velocity, angle) {
+    return {
+        x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+        y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
+    };
+}
+
+function resolveCollision(particle, otherParticle) {
+    var xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
+    var yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
+
+    var xDist = otherParticle.x - particle.x;
+    var yDist = otherParticle.y - particle.y;
+
+    // Prevent accidental overlap of particles
+    if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
+
+        // Grab angle between the two colliding particles
+        var angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
+
+        // Store mass in var for better readability in collision equation
+        var m1 = particle.mass;
+        var m2 = otherParticle.mass;
+
+        // Velocity before equation
+        var u1 = rotate(particle.velocity, angle);
+        var u2 = rotate(otherParticle.velocity, angle);
+
+        // Velocity after 1d collision equation
+        var v1 = { x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2), y: u1.y };
+        var v2 = { x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2), y: u2.y };
+
+        // Final velocity after rotating axis back to original location
+        var vFinal1 = rotate(v1, -angle);
+        var vFinal2 = rotate(v2, -angle);
+
+        // Swap particle velocities for realistic bounce effect
+        particle.velocity.x = vFinal1.x;
+        particle.velocity.y = vFinal1.y;
+
+        otherParticle.velocity.x = vFinal2.x;
+        otherParticle.velocity.y = vFinal2.y;
+    }
+}
+
+module.exports = { randomIntFromRange: randomIntFromRange, randomColor: randomColor, getDistance: getDistance, resolveCollision: resolveCollision, rotate: rotate };
 
 /***/ })
 
--- src/canvas.js
+++ src/canvas.js
@@ -1,71 +1,120 @@
 import utils from './utils'
 
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
 const mouse = {
     x: innerWidth / 2,
     y: innerHeight / 2
-}
+};
 
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
+const colors = ['#2185C5', '#7ECEFD', '#FC0', '#FF7F66'];
 
 // Event Listeners
 addEventListener('mousemove', event => {
-    mouse.x = event.clientX
-    mouse.y = event.clientY
-})
+    mouse.x = event.clientX;
+    mouse.y = event.clientY;
+});
 
 addEventListener('resize', () => {
-    canvas.width = innerWidth
-    canvas.height = innerHeight
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
 
-    init()
-})
+    init();
+});
 
 // Objects
-function Object(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
+function Particle(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.velocity = {
+        x: (Math.random() - 0.5),
+        y: (Math.random() - 0.5)
+    };
+    this.radius = radius;
+    this.color = color;
+    this.mass = 1;
+    this.opacity = 0;
 }
 
-Object.prototype.draw = function() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
-}
+Particle.prototype.draw = function() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.save();
+    c.globalAlpha = this.opacity;
+    c.fillStyle = this.color;
+    c.fill();
+    c.restore();
+    c.strokeStyle = this.color;
+    c.stroke();
+    c.closePath();
+};
 
-Object.prototype.update = function() {
-    this.draw()
-}
+Particle.prototype.update = function(particles) {
+    for (let i = 0; i < particles.length; i++) {
+        this.draw();
+        if (this === particles[i]) continue;
+        if (utils.getDistance(this.x, this.y, particles[i].x, particles[i].y) - this.radius * 2 < 0) {
+            utils.resolveCollision(this, particles[i]);
+        }
+
+        if (this.x - this.radius <= 0 || this.x + this.radius >= innerWidth) {
+            this.velocity.x = -this.velocity.x;
+        }
+
+        if (this.y - this.radius <= 0 || this.y + this.radius >= innerHeight) {
+            this.velocity.y = -this.velocity.y;
+        }
+
+        if (utils.getDistance(mouse.x, mouse.y, this.x, this.y) < 15 && this.opacity < 0.2) {
+            this.opacity += 0.01;
+        } else if (this.opacity > 0) {
+            this.opacity -= 0.01;
+            this.opacity = Math.max(0, this.opacity);
+        }
+
+        this.x += this.velocity.x;
+        this.y += this.velocity.y;
+    }
+};
 
 // Implementation
-let objects
-function init() {
-    objects = []
+let particles = [];
 
-    for (let i = 0; i < 400; i++) {
-        // objects.push();
+function init() {
+    particles = [];
+    initParticles(30);
+}
+
+function initParticles(count) {
+    for (let i = 0; i < count; i++) {
+        const radius = 30;
+        let x = utils.randomIntFromRange(radius, canvas.width - radius);
+        let y = utils.randomIntFromRange(radius, canvas.height - radius);
+        const color = utils.randomColor(colors);
+
+        if (i !== 0) {
+            for (let j = 0; j < particles.length; j++) {
+                if (utils.getDistance(x, y, particles[j].x, particles[j].y) - radius * 2 < 0) {
+                    x = x = utils.randomIntFromRange(radius, canvas.width - radius);
+                    y = utils.randomIntFromRange(radius, canvas.height - radius);
+                    j = -1;
+                }
+            }
+        }
+        particles.push(new Particle(x, y, radius, color));
     }
 }
 
 // Animation Loop
 function animate() {
-    requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
-
-    c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-    // objects.forEach(object => {
-    //  object.update();
-    // });
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    particles.forEach(particle => particle.update(particles));
 }
 
-init()
-animate()
+init();
+animate();
